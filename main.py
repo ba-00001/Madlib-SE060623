d15d5997f32284735fda37f26193b523e5b63fb1
ba00001 <20283986-ba00001@users.noreply.replit.com>
2023-06-06 23:51:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+adjective = input("Whats your favorite adjective? ")
+noun = input("Whats your favorite noun? ")
+animal = input("Whats your favorite animal? ")
+sound = input("Whats your favorite sound? ")
+
+#f-strings for string interpolation
+print(f"The {adjective} {noun} jumped over the {animal} and made a {sound}")
+
+#print("The " + adjective + " " + noun + " jumped over the " + animal + " and made a " + sound)
